4fa79d3391b5d205ca98be313b92a785db805b0b
gewalttat <rbhorses@mail.ru>
2021-08-30 22:39:21 +0500

    root div instead of appendChild

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -3,5 +3,5 @@ import { render } from "react-dom";
 import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
-	render(<App />, document.body.appendChild(document.createElement("div")));
+	render(<App />, document.getElementById('root'));
 });
